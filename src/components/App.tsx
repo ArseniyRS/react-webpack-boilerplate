8a0b203ccb1194c4f21f7bab16e8fb4765cd287a
Arseniy <mrsinkrait@gmail.com>
2022-08-22 19:37:04 +0600

    boilerplate version 0.1

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+const App: React.FC = () => {
+  useEffect(() => {}, []);
+  return (
+    <div className="wrapper">
+      <h1>React 17 and TypeScript 4 App!🚀</h1>
+    </div>
+  );
+};
+export default App;
